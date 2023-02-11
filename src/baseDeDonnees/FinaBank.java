63d3df3c9ef72a05bd2e8503ca9d45c69e0c3402
Yanou Yehiel <85465004+yanouyehiel@users.noreply.github.com>
2023-02-11 22:33:13 +0100

    Add files via upload

---
+++ src/baseDeDonnees/FinaBank.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package baseDeDonnees;
+
+/**
+ *
+ * @author Yepy
+ */
+public class FinaBank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WelcomeFrame accueil = new WelcomeFrame();
+        accueil.setVisible(true);
+    }
+    
+}
